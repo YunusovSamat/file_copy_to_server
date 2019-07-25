e75d1fb9189c97a6ad107ae534cdc67bbab9b355
Yunusov Samat <samat.yunusov@bk.ru>
2019-07-25 13:14:39 +0300

    added multithreading to copy files

--- file_copy_to_server.py
+++ file_copy_to_server.py
@@ -1,5 +1,6 @@
 import json
 import ftplib
+import threading
 
 
 # Класс для копирования файлов с ПК на FTP-сервер.
@@ -11,6 +12,8 @@ class FileCopyFTP:
         self.__password = ''
         self.__paths = []
         self.__conn = ftplib.FTP()
+        # Синхронизация доступа.
+        self.__lock_thread = threading.Lock()
 
     # Метод для чтения данных из json файла.
     def read_json_file(self, path='config.json'):
@@ -61,31 +64,53 @@ class FileCopyFTP:
             else:
                 print('Вход выполнен.')
 
-    # Метод для копирования файлов на сервер.
-    def copy_to_server(self):
-        # Перебор всех файлов, которые должны скопироваться на ftp-сервер.
-        for file in self.__paths:
+    # Метод для копирования файла на ftp-сервер.
+    def copy_file_to_server(self, source_path, ftp_path):
+        # Блокировка для других потоков.
+        with self.__lock_thread:
             try:
                 # Открываем файл копируемого на чтение в бинарном режиме.
-                with open(file['source_path'], 'rb') as source_file:
+                with open(source_path, 'rb') as source_file:
                     # Отправляем файл на ftp-сервер.
-                    self.__conn.storbinary('STOR ' + file['ftp_path'],
+                    self.__conn.storbinary('STOR ' + ftp_path,
                                            source_file)
             # Обработка исключения, если копируемый файл не найден.
             except FileNotFoundError:
-                print('Файл не найден')
+                print('Файл не найден ', source_path)
             # Обработка исключения, если указана папка, а не файл.
             except IsADirectoryError:
-                print('Это не файл, а папка')
-            # Обработка исключения, если возникла другая ошибка файла.
-            except OSError:
-                print('Ошибка открытия файла')
-            # Обработка исключения, если возникла ошбика ftp-сервера.
+                print('Это не файл, а папка ', source_path)
+                # Обработка исключения, если возникла ошбика ftp-сервера.
             except ftplib.error_perm as err:
                 print('Ошибка: ', err)
+            # Обработка исключения, если возникла другая ошибка файла.
+            except OSError as err:
+                print('Ошибка:', err)
+
             # Если файл успешно скопирован на ftp-сервер.
             else:
-                print('Успешно скопирован файл:', file['source_path'])
+                print('Успешно скопирован файл:', source_path)
+
+    # Метод для перебора всех файлов.
+    def threads_copy_files(self):
+        # Список всех потоков
+        thread_list = []
+        # Перебор всех файлов, которые должны скопироваться на ftp-сервер.
+        for file in self.__paths:
+            # Инициализируем поток
+            thread_temp = threading.Thread(
+                target=self.copy_file_to_server,
+                name=file['source_path'],
+                args=(file['source_path'], file['ftp_path'])
+            )
+            # Добавляем поток в список
+            thread_list.append(thread_temp)
+            # Запуск потока
+            thread_temp.start()
+
+        # Дожидаемся конца всех потоков
+        for thread_temp in thread_list:
+            thread_temp.join()
 
     # Вывод информации хоста, порта и пользователя.
     def __str__(self):
@@ -111,5 +136,5 @@ if __name__ == '__main__':
     FILE_COPY_FTP.read_json_file()
     # Устанавливаем соединение с ftp-сервером.
     FILE_COPY_FTP.connect_server()
-    # Копируем файлы на ftp-сервер.
-    FILE_COPY_FTP.copy_to_server()
+    # Запускаем потоки для копирования файлов на ftp-сервер.
+    FILE_COPY_FTP.threads_copy_files()
